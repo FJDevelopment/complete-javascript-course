988a900ea470e8a238e82db283872f07d9beb240
FJDevelopment <francisjordandev@gmail.com>
2025-06-23 20:27:55 +0100

    completed unit 5 - Guess my number

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,58 @@
 'use strict';
+
+let secretNumber;
+let score = 20;
+let hiScore = 0;
+const message = document.querySelector('.message');
+const userInput = document.querySelector('.guess');
+const middleNumber = document.querySelector('.number');
+const scoreElement = document.querySelector('.score');
+const hiScoreElement = document.querySelector('.highscore');
+
+const checkGuess = async () => {
+  const guess = userInput.value;
+  const intguess = Number(guess);
+  if (intguess == secretNumber) {
+    middleNumber.textContent = secretNumber;
+    message.textContent = 'yeah baby you got the number!';
+    winRound();
+    await new Promise(r => setTimeout(r, 2000));
+    score = 20;
+    beginFreshRound();
+  } else {
+    message.textContent =
+      intguess > secretNumber ? 'Too high bby' : 'Too low bby';
+    score--;
+    scoreElement.textContent = score;
+  }
+};
+
+const beginFreshGame = () => {
+  score = 20;
+  hiScore = 0;
+  hiScoreElement.textContent = hiScore;
+
+  beginFreshRound();
+};
+
+const beginFreshRound = () => {
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  middleNumber.textContent = '?';
+  message.textContent = 'Guess My Number!';
+  userInput.value = '';
+  scoreElement.textContent = score;
+  console.log(secretNumber + 'is the number');
+};
+
+const winRound = () => {
+  if (score > hiScore) {
+    hiScore = score;
+    hiScoreElement.textContent = hiScore;
+  }
+  scoreElement.textContent = score;
+};
+
+beginFreshGame();
+
+document.querySelector('.check').addEventListener('click', checkGuess);
+document.querySelector('.again').addEventListener('click', beginFreshGame);
